a8d97d7c7bfe09228eb7256efc983cc1f790c728
jkowalski995 <kuba.ko.1995@gmail.com>
2023-04-07 10:17:07 +0200

    Little update for API Key read from file

--- gui.py
+++ gui.py
@@ -8,17 +8,16 @@ import time
 from io import BytesIO
 
 
-def api_call(url=None, api_key=None):
+def api_call(url=None):
     """
     Will connect with urlscan.io API and retrieve an image (bytes) of provided url.
     :param url: (string) a string url of scanned website with or without https://www.
     :return: (bytes) image view in bytes object
     """
-    if api_key is None:
-        with open('config.txt') as config:
-            api_key = config.readline()
+    with open('config.txt', 'r') as config:
+        api_key = config.readline()[:-1]
 
-    headers = {'API-Key': str(api_key) , 'Content-Type': 'application/json'}
+    headers = {'API-Key': api_key, 'Content-Type': 'application/json'}
     data = {"url": url, "visibility": "private"}
     response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
     print(response)
